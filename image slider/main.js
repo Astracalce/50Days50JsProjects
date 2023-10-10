22c47efb14e4225c881650c66886b09a0d0e092c
astracalce <shakirmustehsin@gmail.com>
2023-10-10 17:55:22 +0530

    Initial commit

---
+++ image slider/main.js
@@ -0,0 +1,20 @@
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel)=> {
+
+    panel.addEventListener('click', () => {
+            removeActive();
+            panel.classList.add('active');
+    
+        
+    })
+    
+
+})
+
+function removeActive (panel) {
+    panels.forEach(panel => {
+        panel.classList.remove('active');
+    })
+    
+}
